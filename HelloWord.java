9cb17aa902fc16249d907c6c93a468df26716d61
SatyakiGhosh086 <82649842+SatyakiGhosh086@users.noreply.github.com>
2023-05-04 10:58:27 +0530

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWord
   public static void main(String args[])
   {
     System.out.println("Hello World!!");
+    System.out.println("This is a demo for Jenkins and Git Integration");
   }
 }
